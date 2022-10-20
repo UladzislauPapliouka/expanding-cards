9f39a24243660b3e05966ed92e8103de9d624050
Uladzislau Papliouka <40912110+UladzislauPapliouka@users.noreply.github.com>
2022-10-20 18:28:16 +0300

    Delete expanding-card directory

--- expanding-card/script.js
+++
@@ -1,9 +0,0 @@
-const slides = document.querySelectorAll(".slide")
-
-document.addEventListener("click", event => {
-  const node = event.target
-  if(node.classList.contains("slide") && !node.classList.contains("active")){
-    slides.forEach((slide) =>slide.classList.remove("active"))
-    node.classList.add("active")
-  }
-})
